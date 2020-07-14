6319e02a07d9edd3d4fb83333f8c4865177123cc
Jonathan Sharp <39886295+jonathansharp@users.noreply.github.com>
2020-07-14 10:46:36 -0500

    Remove _extd from filename

--- CO2SYS.m
+++ CO2SYS.m
@@ -1322,7 +1322,7 @@ if any(F)
 	% Data used in this work is from:
 	% K1: Merhback (1973) for S>15, for S<15: Mook and Keone (1975)
 	% K2: Merhback (1973) for S>20, for S<20: Edmond and Gieskes (1970)
-	% Sigma of residuals between fits and above data: ±0.015, +0.040 for K1 and K2, respectively.
+	% Sigma of residuals between fits and above data: Â±0.015, +0.040 for K1 and K2, respectively.
 	% Sal 0-40, Temp 0.2-30
   % Limnol. Oceanogr. 43(4) (1998) 657-668
 	% On the NBS scale
